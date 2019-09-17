21d76bce8311786cedd8990ca0eae2987c913ed4
Jacob Lewis <101623288@student.swin.edu.au>
2019-09-17 17:32:44 +1000

    added salesassistant role

---
+++ PHP-SRePS/PHP-SRePS/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace PHP_SRePS.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<PHP_SRePS.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(PHP_SRePS.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
